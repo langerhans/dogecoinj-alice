26e18762561223fa4cd4472521c1f2120d92b691
langerhans <max.keller@gmx.com>
2014-08-28 19:29:10 +0200

    Fix message signing

--- core/src/main/java/com/google/dogecoin/core/Utils.java
+++ core/src/main/java/com/google/dogecoin/core/Utils.java
@@ -50,7 +50,7 @@ public class Utils {
     }
 
     /** The string that prefixes all text messages signed using Bitcoin keys. */
-    public static final String BITCOIN_SIGNED_MESSAGE_HEADER = "Bitcoin Signed Message:\n";
+    public static final String BITCOIN_SIGNED_MESSAGE_HEADER = "Dogecoin Signed Message:\n";
     public static final byte[] BITCOIN_SIGNED_MESSAGE_HEADER_BYTES = BITCOIN_SIGNED_MESSAGE_HEADER.getBytes(Charsets.UTF_8);
 
     // TODO: Replace this nanocoins business with something better.
